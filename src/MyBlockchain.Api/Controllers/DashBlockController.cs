3536a4d61baed2f2641f9e8639d55cb9853f5bd0
Nikhil <nmokariya@gmail.com>
2025-09-28 03:13:44 +0530

    latest changes for CRUD operations in Dashblock related controller and API endpoints

--- src/MyBlockchain.Api/Controllers/DashBlockController.cs
+++ src/MyBlockchain.Api/Controllers/DashBlockController.cs
@@ -46,5 +46,63 @@ namespace MyBlockchain.Api.Controllers
 
             return Ok(blockHistory);
         }
+
+
+        // Common Methods if required 
+        // CRUD operations with GetAll 
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var dashBlocks = await _dashBlockService.GetAllAsync();
+            return Ok(dashBlocks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var dashBlock = await _dashBlockService.GetByIdAsync(id);
+            if (dashBlock == null) return NotFound();
+            return Ok(dashBlock);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(DashBlock dashBlock)
+        {
+            var created = await _dashBlockService.AddAsync(dashBlock);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, DashBlock dashBlock)
+        {
+            if (id != dashBlock.Id) return BadRequest();
+
+            try
+            {
+                await _dashBlockService.UpdateAsync(dashBlock);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _dashBlockService.DeleteAsync(id);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+
+        // CRUD Operations completed
     }
 }
