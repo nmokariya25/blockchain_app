6cceeb6021774fd5634c93ab65462790ff0a554e
Nikhil <nmokariya@gmail.com>
2025-09-27 22:19:24 +0530

    Resolved functional test cases

--- tests/MyBlockChain.Tests.Functional/CustomWebApplicationFactory.cs
+++ tests/MyBlockChain.Tests.Functional/CustomWebApplicationFactory.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyBlockchain.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +16,43 @@ namespace MyBlockChain.Tests.Functional
 {
     public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
     {
-        protected override IHost CreateHost(IHostBuilder builder)
+        private readonly SqliteConnection _connection;
+
+        public CustomWebApplicationFactory()
         {
-            builder.UseContentRoot(GetApiProjectPath());
-            return base.CreateHost(builder);
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
         }
 
-        private string GetApiProjectPath()
-        {   
-            var dir = Directory.GetCurrentDirectory();
-            var path = Path.Combine(dir, "..", "..", "..", "..","..", "src", "MyBlockChain.API");
-            return Path.GetFullPath(path);
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Remove the app's real DbContext registration
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<BlockCypherDbContext>));
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                // Add test DbContext using the in-memory SQLite
+                services.AddDbContext<BlockCypherDbContext>(options =>
+                {
+                    options.UseSqlite(_connection);
+                });
+
+                // Build the service provider and create the schema
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<BlockCypherDbContext>();
+                db.Database.EnsureCreated(); // Or db.Database.Migrate() if using migrations
+            });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _connection.Close(); // Important: clean up the connection
         }
     }
+
 }
