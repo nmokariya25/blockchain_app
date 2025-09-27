1e25312abebd6b029ad69d8982ce22be42774aad
Nikhil <nmokariya@gmail.com>
2025-09-27 23:35:46 +0530

    remove unnecessary references of the projects and dependencies

--- src/MyBlockchain.Api/Controllers/EthBlockController.cs
+++ src/MyBlockchain.Api/Controllers/EthBlockController.cs
@@ -13,13 +13,11 @@ namespace MyBlockchain.Api.Controllers
     public class EthBlockController : ControllerBase
     {
         private readonly IEthBlockService _ethService;
-        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
-        public EthBlockController(IEthBlockService ethService, IUnitOfWork unitOfWork, IMapper mapper)
+        public EthBlockController(IEthBlockService ethService, IMapper mapper)
         {
             _ethService = ethService;
-            _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
 
@@ -30,8 +28,7 @@ namespace MyBlockchain.Api.Controllers
             if (dto == null) return NotFound("No data received from API.");
 
             var entity = _mapper.Map<EthBlock>(dto);
-            await _unitOfWork.EthBlocks.AddAsync(entity);
-            await _unitOfWork.CompleteAsync();
+            await _ethService.AddAsync(entity);
 
             return Ok(new { message = "Block data saved successfully", blockHeight = entity.Height });
         }
@@ -39,7 +36,7 @@ namespace MyBlockchain.Api.Controllers
         [HttpGet("latest-block/{count?}")]
         public async Task<IActionResult> GetLatestBlock(int count = 0)
         {
-            var latestBlock = await _unitOfWork.EthBlocks.GetAllAsync();
+            var latestBlock = await _ethService.GetAllAsync();
 
             var blockHistory = (count > 0)
                 ? latestBlock.OrderByDescending(b => b.CreatedAt).Take(count)
